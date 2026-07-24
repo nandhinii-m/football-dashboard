4924719e7b973fda5b6cd402ae86c481c1d37d01
Nandhini <srinandhinilakshmi07@gmail.com>
2026-07-24 22:27:49 +0530

    fin1

--- 🏠_home.py
+++ 🏠_home.py
@@ -11,7 +11,7 @@ st.title("Football Analytics Dashboard")
 
 
 st.write("""
-This dashboard allows you to analyze player and club statistics from the **2025–2026 season across top five European leagues**.
+This dashboard allows you to analyze player and club statistics from the **2025–2026 season across the top five European leagues**.
 """)
 st.divider()
 
