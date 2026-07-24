9e28d09fe60fdd9785127372dc0cc9f75bf87eda
Nandhini <srinandhinilakshmi07@gmail.com>
2026-07-24 22:19:58 +0530

    fin1

--- 🏠_home.py
+++ 🏠_home.py
@@ -7,30 +7,12 @@ st.set_page_config(
     initial_sidebar_state="auto"
 )
 
-st.title("⚽ Football Analytics Dashboard")
+st.title("Football Analytics Dashboard")
 
-st.divider()
 
 st.write("""
-This dashboard allows you to analyze player and club statistics from the **2025–2026 season across Europe's Top Five Leagues**.
+This dashboard allows you to analyze player and club statistics from the **2025–2026 season across top five European leagues**.
 """)
-
-st.header("Features")
-
-st.markdown("""
-📊 **League Analysis** -
-Discover the top scorers, assist providers,
-and highest scoring clubs in each league.
-
-⭐ **Player Search** - 
-Search any player and view detailed statistics
-tailored to their position.
-
-👥 **Player Comparison** - 
-Compare two players side by side and
-see who comes out on top.
-""")
-
 st.divider()
 
 st.caption("Dataset : Kaggle")
